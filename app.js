a6f4cc59c7a4aede9c70d422066a49d33f01c035
karthikeyan N P <karthick.psgtec12@gmail.com>
2018-03-19 15:13:16 +0530

    first update

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ module.exports = {
 
   basicTest: () => {
     return 'hello world';
+  },
+  basicTypeTest: () => {
+    return 1;
   }
-
 };
--- test/appTest.js
+++ test/appTest.js
@@ -2,10 +2,15 @@ const assert = require('chai').assert;
 const app = require('../app');
 
 basicTestResult = app.basicTest();
+basicTypeTestResult = app.basicTypeTest();
 describe('App', () => {
 
   it('app should return hello world', () => {
     assert.equal(basicTestResult, 'hello world');
+  });
+
+  it('app should return type string', () => {
+    assert.typeOf(basicTypeTestResult, 'string');
   });
 
 });
